b345714be714a2e1f9ee44c99172cfb200df15ef
JasonLam0990 <jasonlam0990@gmail.com>
2019-12-11 09:53:01 +0800

    瞎搞了几行

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 //app.js
 App({
   onLaunch: function () {
+    console.log('1111111')
     // 展示本地存储能力
     var logs = wx.getStorageSync('logs') || []
     logs.unshift(Date.now())
@@ -19,8 +20,8 @@ App({
           // 已经授权，可以直接调用 getUserInfo 获取头像昵称，不会弹框
           wx.getUserInfo({
             success: res => {
+              console.log('222222')
               // 可以将 res 发送给后台解码出 unionId
-              this.globalData.userInfo = res.userInfo
 
               // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
               // 所以此处加入 callback 以防止这种情况
